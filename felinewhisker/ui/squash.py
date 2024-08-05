37375f23272c0da0e780cb5656d94799b515868a
narugo1992 <narugo992@gmail.com>
2024-08-06 05:43:57 +0800

    dev(narugo): optimize squash page

--- felinewhisker/ui/squash.py
+++ felinewhisker/ui/squash.py
@@ -17,6 +17,8 @@ def create_squash_tab(
                 pass
 
     def _fn_data_load():
+        yield gr.update(), gr.update(), \
+            gr.update(interactive=False, value='Loading'), gr.update(interactive=False),
         table = repo.read_table()
         if table is None:
             table = pd.DataFrame([])
@@ -25,30 +27,40 @@ def create_squash_tab(
         for _, df in repo.read_unarchived_tables():
             records.extend(df.to_dict('records'))
 
-        return table, pd.DataFrame(records)
+        yield table, pd.DataFrame(records), \
+            gr.update(interactive=True, value='Refresh'), gr.update(interactive=True)
 
     def _fn_squash():
+        yield gr.update(interactive=False), gr.update(interactive=False, value='Squashing')
         gr.Info('Squashing ...')
         repo.squash()
+        yield gr.update(interactive=True), gr.update(interactive=True, value='Squash')
         gr.Info('Squashed!')
 
+    def _fn_init():
+        return gr.update(interactive=True), gr.update(interactive=True)
+
     with gr.Row():
-        gr_refresh = gr.Button('Refresh')
-        gr_squash = gr.Button('Squash', variant='primary')
+        gr_refresh = gr.Button('Refresh', interactive=False)
+        gr_squash = gr.Button('Squash', variant='primary', interactive=False)
 
         gr_refresh.click(
             fn=_fn_data_load,
-            outputs=[gr_table, gr_unarchived_table],
+            outputs=[gr_table, gr_unarchived_table, gr_refresh, gr_squash],
         )
 
         gr_squash.click(
             fn=_fn_squash,
+            outputs=[gr_refresh, gr_squash]
         ).then(
             fn=_fn_data_load,
-            outputs=[gr_table, gr_unarchived_table],
+            outputs=[gr_table, gr_unarchived_table, gr_refresh, gr_squash],
         )
 
     demo.load(
         fn=_fn_data_load,
-        outputs=[gr_table, gr_unarchived_table],
+        outputs=[gr_table, gr_unarchived_table, gr_refresh, gr_squash],
+    ).then(
+        fn=_fn_init,
+        outputs=[gr_refresh, gr_squash],
     )
