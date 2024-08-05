e04c3a705e1019d5cabe2aa0ee52a8f248db1325
narugo1992 <narugo992@gmail.com>
2024-08-06 06:20:06 +0800

    dev(narugo): update UI

--- felinewhisker/ui/dispatch.py
+++ felinewhisker/ui/dispatch.py
@@ -1,3 +1,4 @@
+import logging
 import os
 import pathlib
 from contextlib import contextmanager
@@ -6,6 +7,8 @@ from typing import Optional, ContextManager, Callable, Any
 import gradio as gr
 from hbutils.string import titleize
 from hbutils.system import TemporaryDirectory
+from hfutils.operate import get_hf_client
+from huggingface_hub.errors import LocalTokenNotFoundError
 
 from .annotate import create_annotation_tab
 from .squash import create_squash_tab
@@ -21,6 +24,16 @@ def create_annotator_app(
         fn_annotate_assist: Optional[Callable[[str], Any]] = None,
         annotation_options: Optional[dict] = None
 ) -> ContextManager[gr.Blocks]:
+    hf_client = get_hf_client()
+
+    if not author:
+        try:
+            info = hf_client.whoami()
+            author = info['name']
+        except LocalTokenNotFoundError:
+            logging.warning('Huggingface auth failed, no author name used, this session will run as guest.')
+            author = None
+
     with TemporaryDirectory(prefix='felinewhisker_') as td_state, \
             repo.write(author=author) as write_session, datasource as source:
         state_file = os.path.join(td_state, 'state.json')
@@ -28,12 +41,27 @@ def create_annotator_app(
 
         with gr.Blocks(css=_GLOBAL_CSS_CODE) as demo:
             with gr.Row(elem_id='annotation_title'):
-                gr_title = gr.HTML(
-                    f'<p class="title">'
-                    f'<u>{titleize(repo.meta_info["task"])}</u> - {repo.meta_info["name"]}'
-                    f'</p>'
-                )
-                _ = gr_title
+                with gr.Column():
+                    gr_title = gr.HTML(
+                        f'<p class="title">'
+                        f'<u>{titleize(repo.meta_info["task"])}</u> - {repo.meta_info["name"]}'
+                        f'</p>'
+                    )
+                    _ = gr_title
+
+                    if author:
+                        gr_subtitle = gr.HTML(
+                            f'<p class="subtitle">'
+                            f'Hello, <u>@{author}</u>!'
+                            f'</p>'
+                        )
+                    else:
+                        gr_subtitle = gr.HTML(
+                            f'<p class="subtitle negative">'
+                            f'(<b>Warning</b>: Running in Guest Mode)'
+                            f'</p>'
+                        )
+                    _ = gr_subtitle
 
             with gr.Row():
                 with gr.Tabs():
